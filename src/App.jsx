f550992250997e15592cca800ec5737c25ba4588
bisre1921 <bisrattewodros3@gmail.com>
2024-04-23 08:55:38 +0300

    Implement layout in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import { BrowserRouter as Router } from "react-router-dom";
+import {About , Contact , Experience , Feedbacks , Hero , Navbar , Tech , Works , StarsCanvas} from "./components";
+
+const App = () =>  {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </Router>
   )
 }
+
+export default App;
